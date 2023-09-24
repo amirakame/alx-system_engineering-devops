b418217757b3c28a268d4ad1ed3522c69fc6c6b5
amirakame <amirakamel343@gmail.com>
2023-09-24 12:10:08 -0700

    chool

--- 0x0B-ssh/script.sh
+++ 0x0B-ssh/script.sh
@@ -2,10 +2,10 @@
 # This script checks if all files in the current directory end with a new line
 
 for file in *; do
-    if [[ -f $file ]]; then
-            last_char=$(tail -c 1 "$file")
-	            if [[ $last_char != $'\n' ]]; then
-		                echo "File $file does not end with a new line"
-				        fi
-					    fi
-					    done
+if [[ -f $file ]]; then
+last_char=$(tail -c 1 "$file")
+if [[ $last_char != $'\n' ]]; then
+echo "File $file does not end with a new line"
+fi
+fi
+done
